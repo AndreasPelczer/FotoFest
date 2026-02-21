28740c3fae59fc6f8b21d5e0c8bd6e73acea2033
Andreas Pelczer <134264285+AndreasPelczer@users.noreply.github.com>
2026-02-21 12:01:55 +0100

    Add files via upload

---
+++ FotoFest/FotoFestTests/FotoFestTests.swift
@@ -0,0 +1,17 @@
+//
+//  FotoFestTests.swift
+//  FotoFestTests
+//
+//  Created by Andreas Pelczer on 21.02.26.
+//
+
+import Testing
+@testable import FotoFest
+
+struct FotoFestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
